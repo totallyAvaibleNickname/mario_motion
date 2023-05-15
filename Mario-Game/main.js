a9f54f70b480f2c7b20c92f4ff3c1d56846da23b
Matheus Rinaldi <132310788+totallyAvaibleNickname@users.noreply.github.com>
2023-05-15 20:02:15 -0300

    Add files via upload

---
+++ Mario-Game/main.js
@@ -0,0 +1,35 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('gameConsole');
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game();
+}
+function modelLoaded() {
+	console.log('Modelo Carregado');
+}
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
